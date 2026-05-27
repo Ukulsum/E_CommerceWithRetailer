7c47eb5b47a6029d3be126178aeb8792f3994cdf
ukulsum <ukulsum1280@gmail.com>
2026-05-27 17:14:53 +0600

    CompanyInfoApiController

---
+++ POS/Controllers/Files/Api/CompanyController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace POS.Controllers.Files.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+    }
+}
